8e6f6a376d35b883ba026b99163ee63491b5344a
jesscianyquist <jessicanyquist3@gmail.com>
2016-07-10 23:02:47 +0200

    Day 10 Assignment

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ var app = angular.module('Instamood',[]);
 
 app.controller('MainCtrl', function($scope, $http) {
   // get the access token if it exists
+var ACCESS_TOKEN = "177538508.7e5ca69.db9e056c9ef94ababbaa04435a5ace5b";
+// $scope.ego[];
+// $scope.pop[];
+// $scope.active[];
+$scope.brevity = 0;
+// $scope.vis[];
+$scope.likes = 0;
+$scope.hashtag = 0;
+$scope.hasLiked = 0;
+// $scope.days[];
 	$scope.hasToken = true;
 	var token = window.location.hash;
 	console.log(token);
@@ -18,13 +28,31 @@ app.controller('MainCtrl', function($scope, $http) {
 	    method: "JSONP",
 	    url: mediaUrl,
 	    params: {
-	    	callback: "JSON_CALLBACK"
+	    	'callback': "JSON_CALLBACK",
+	    	'access_token': ACCESS_TOKEN
         // you need to add your access token here, as per the documentation
 	    }
 	  }).then(function(response) {
       $scope.picArray = response.data.data;
-      // now analyze the sentiments and do some other analysis
-      // on your images 
+
+      console.log(response.data.data);
+      for(var i = 0; i < $scope.picArray.length; i++){
+      	$scope.likes = $scope.likes + $scope.picArray[i].likes.count;
+      	if($scope.picArray[i].user_has_liked){
+      		$scope.hasLiked++;
+      	}
+      	console.log($scope.hasLiked);
+      	var words_arr = $scope.picArray[i].caption.text.split(" ");
+      	$scope.brevity = $scope.brevity + words_arr.length;
+      	$scope.hashtag = $scope.hashtag + $scope.picArray[i].tags.length;
+      	console.log($scope.hashtag);
+
+      };
+      $scope.average = $scope.likes * 1.0 / $scope.picArray.length;
+      $scope.ego = $scope.hasLiked / $scope.picArray.length;
+      $scope.brevity = $scope.brevity / $scope.picArray.length;
+      $scope.hashtag = $scope.hashtag / $scope.picArray.length;
+      console.log($scope.hastag)
 	  })
 	};
 
